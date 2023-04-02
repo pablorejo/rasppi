9d11abccb19e7aeae173492ec5dab5e15489447c
pablorejo <pablopiorejoiglesias@gmail.com>
2023-04-02 19:29:29 +0200

    Comentando los scripts

--- home/programas/scripts/init_rasp.sh
+++ home/programas/scripts/init_rasp.sh
@@ -4,14 +4,14 @@ home=/home/pi
 #actualizar pip
 sudo apt-get install python3-pip -y
 
-#actualizar pyTelegramBotAPI
-pip install pyTelegramBotAPI
+# #actualizar pyTelegramBotAPI
+# pip install pyTelegramBotAPI
 
-#iniciar bot de python
-programas=$home/programas
-cd $programas
-echo "iniciando bot de telegram ip.py"
-python3 bots_telegram/bot_ip/ip.py & > bot_ip.txt
+# #iniciar bot de python
+# programas=$home/programas
+# cd $programas
+# echo "iniciando bot de telegram ip.py"
+# python3 bots_telegram/bot_ip/ip.py & > bot_ip.txt
 
 
 #iniciar pihole
